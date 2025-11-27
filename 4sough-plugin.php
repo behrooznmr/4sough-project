21d6aa3b19d1bef88539d21e7f6971e3e36ab184
Behrooz <behrooznm5@gmail.com>
2025-11-27 13:08:30 +0330

    git init

---
+++ 4sough-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: پلاگین اختصاصی چهارسوق - ویرایشات اختصاصی
+Plugin URI: https://raman.agencu
+Description: ویرایشات اختصاصی تیم برنامه نویسی رامان برای سایت بلاگ 4 سوق
+Version: 1.0
+Author: بهروز نعمت مراد
+Author URI: https://raman.agencu
+License: A "Slug" license name e.g. GPL2
+*/
